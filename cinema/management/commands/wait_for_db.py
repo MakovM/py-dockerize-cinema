e5e57d655de34f8eb2559da81d49341dee9fd04c
MakovM <makovinskiy09@gmail.com>
2025-09-14 15:13:38 +0300

    Solution

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -11,8 +11,7 @@ class Command(BaseCommand):
         db_conn = None
         while not db_conn:
             try:
-                db_conn = connections["default"]
-                c = db_conn.cursor()
+                db_conn = connections["default"].cursor()
             except OperationalError:
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
